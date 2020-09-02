1854d63ed4fd239e26e5ed481f01b6a85837ccd6
Matthew Mogannam <matthewmogannam@icloud.com>
2020-09-01 21:13:18 -0700

    Built the app

---
+++ flix/Base.lproj/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flix
+//
+//  Created by Matthew Mogannam on 9/1/20.
+//  Copyright © 2020 Matthew Mogannam. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
